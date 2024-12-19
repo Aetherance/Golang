1bf66fb17cc2763162265cb16616a58735a25626
User <2085163736@qq.com>
2024-12-19 17:31:18 +0800

    up

--- 1.go
+++ 1.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func add(x int,y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Println("Hello, 世界")
+	var x = 1
+	var y = 2
+	var a , b , c = 1 , 2 , 3
+	fmt.Println(a,"\n",b,"\n",c)
+	fmt.Println(add(x,y))
+	fmt.Println("Hello World\n"+"Hello Linux",x)
 }
