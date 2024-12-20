25a9b39207b90d717b5f3849664e07fcf27ceb1b
User <2085163736@qq.com>
2024-12-20 18:31:57 +0800

    update files

--- 1.go
+++ 1.go
@@ -1,16 +1,15 @@
 package main
 
 import "fmt"
+import "C"
 
 func add(x int,y int) int {
 	return x + y
 }
 
-func main() {
-	var x = 1
-	var y = 2
-	var a , b , c = 1 , 2 , 3
-	fmt.Println(a,"\n",b,"\n",c)
-	fmt.Println(add(x,y))
-	fmt.Println("Hello World\n"+"Hello Linux",x)
+func main(){
+	
+	a := 10
+	fmt.Printf("%d",a);
+	
 }
